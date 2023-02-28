0aca7ab47f3bc434ac5018aaa2e6ffd25d60e7d8
Frederik Zwartbol <fzwartbol@anwb.nl>
2023-02-28 20:14:49 +0100

    - changed application.yml for different env

--- src/main/java/com/frederikzwartbol/springboottwitterrebuild/SpringbootTwitterRebuild.java
+++ src/main/java/com/frederikzwartbol/springboottwitterrebuild/SpringbootTwitterRebuild.java
@@ -1,11 +1,9 @@
 package com.frederikzwartbol.springboottwitterrebuild;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class SpringbootTwitterRebuild
 {
     public static void main(String[] args) {
